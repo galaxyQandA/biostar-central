ffd3ac7270ce0c3dbce39ff885956dd480616c8d
galaxyQandA <galaxyqanda@gmail.com>
2012-08-16 15:43:03 -0400

    Settings for postgre on Slyfox

--- main/settings.py
+++ main/settings.py
@@ -55,10 +55,10 @@ PLANET_DIR    = path(HOME_DIR, 'db', 'planet')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': DATABASE_NAME,                  # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'biostar',                  # Or path to database file if using sqlite3.
+        'USER': 'biostar',                      # Not used with sqlite3.
+        'PASSWORD': 'ZG7SPwBfNKr5Bnn',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
