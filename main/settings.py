5417b057d965809f8db5e068bdca3cfead63c4eb
galaxyQandA <galaxyqanda@gmail.com>
2012-09-10 13:52:32 -0400

    Merge from upstream

--- main/settings.py
+++ main/settings.py
@@ -57,8 +57,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'biostar',                  # Or path to database file if using sqlite3.
-        'USER': 'biostar',                      # Not used with sqlite3.
-        'PASSWORD': 'ZG7SPwBfNKr5Bnn',                  # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'galaxy',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
